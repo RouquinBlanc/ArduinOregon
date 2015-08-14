8eff94779579d6d265ceb9c91f26548f0f2ad52b
Jonathan Martin <homerunisgood@hotmail.com>
2015-08-14 11:26:25 +0200

    [doc] add proper license to MPL3115A2 files

--- libraries/MPL3115A2/MPL3115A2.h
+++ libraries/MPL3115A2/MPL3115A2.h
@@ -1,3 +1,26 @@
+/*
+ * MPL3115A2 Altitude/Pressure Sensor Library
+ *
+ * Copyright (C) 2015 Jonathan Martin <therouquinblanc@gmail.com>
+ * Based on original work from:
+ *   - A.Weiss, 7/17/2012
+ *   - Nathan Seidle, 9/23/2013
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #ifndef __MPL3115A2__
 #define __MPL3115A2__
 
